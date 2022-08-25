60ad0c7fd3672ff9f5b2b5dac6039f5756450331
George <nanjing1222@163.com>
2022-08-25 15:04:33 +0800

    add test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -32,6 +34,18 @@ public class Test {
         }).start();
 
         //Java开不了线程，通过调用本地方法(native method 底层的C++)调用
+
+        //New, RUNNABLE,BLOCKED, TIMED_WAITING, WAITING, TERMINATED
+        try {
+            //睡一天
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //137 6471 6656 官希标
+
+
     }
 
 
